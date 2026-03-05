8ed6523e1b2ddd83c4c65f2eda0deacfda7607a8
Vikramjeetblog <89984187+Vikramjeetblog@users.noreply.github.com>
2026-03-05 17:49:56 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import Navbar from "../components/Navbar"
+import Hero from "../components/Hero"
+import AIConcept from "../components/AIConcept"
+import Vision from "../components/Vision"
+import Academy from "../components/Academy"
+import Waitlist from "../components/Waitlist"
+import Footer from "../components/Footer"
+
+export default function Home() {
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <AIConcept />
+      <Vision />
+      <Academy />
+      <Waitlist />
+      <Footer />
+    </>
+  )
+
+}

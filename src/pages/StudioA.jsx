371e535a615ead06c4d92dd2b906cd7f3d83fe12
Vikramjeetblog <89984187+Vikramjeetblog@users.noreply.github.com>
2026-04-25 14:57:00 +0530

    Update StudioA.jsx

--- src/pages/StudioA.jsx
+++ src/pages/StudioA.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import StudioHero from "../components/studio/studioA/StudioHero"
 import Equipment from "../components/studio/studioA/Equipment"
 import StudioIntro from "../components/studio/studioA/StudioIntro"
-import StudioCTA from "../components/studioCTA";
+import StudioCTA from "../components/StudioCTA";
 
 const StudioA = () => {
 	return (
